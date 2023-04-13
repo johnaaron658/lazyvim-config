5f0291714187b1efcc65a6f6df1ae62d68072348
John Aaron Q. Alcoseba <johnaaron658@gmail.com>
2023-04-13 20:48:01 +0800

    added keymaps and updated plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+local function map(mode, lhs, rhs, opts)
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+
+map("n", "<tab>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
